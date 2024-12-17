f0f82afea96fa533be8b546e178c0603334dc0ee
CoderShuaib100 <sayeedishuaib@gmail.com>
2024-12-17 20:59:35 +0530

    bye

--- HOLLOW_PATTERNER.py
+++ HOLLOW_PATTERNER.py
@@ -3,9 +3,7 @@ rows = int(input("Enter the number of rows: "))
 cols = int(input("Enter the number of columns: "))
 for i in range(1,rows + 1):
     for j in range(1,cols + 1):
-        if (i == j):
-            print(" * ",end="")
-        if(i == 1 or i == rows or j == 1 or j == cols):
+        if(i == 1 or i == rows or j == 1 or j == cols or i == j):
             print(" * ",end="")
         else:
             print("   ",end="")
